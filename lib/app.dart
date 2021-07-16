0290f4df138a59932e46cc02845bfb4414ab795b
JBPort94 <49971764+JBPort94@users.noreply.github.com>
2021-07-15 22:16:54 -0600

    Column for Business Card started - Playing with spacer containers. Need to investigate MainAxisAlignment properties.

--- lib/app.dart
+++ lib/app.dart
@@ -9,9 +9,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Call Me Maybe',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.blueGrey,
       ),
       home: MainTabController()
     );
--- lib/screens/business_card_screen.dart
+++ lib/screens/business_card_screen.dart
@@ -3,8 +3,47 @@ import 'package:flutter/material.dart';
 class BusinessCardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Business Card')
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 8,
+              ),
+              CircleAvatar(
+                radius: avatarSize(context),
+                backgroundColor: Colors.black,
+              ),
+              Text(
+                'Jeff Porter',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Jeff Porter',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      )
     );
+  }
+
+  double avatarSize(BuildContext context) {
+    if (MediaQuery.of(context).orientation == Orientation.landscape) {
+      return MediaQuery.of(context).size.width * 0.1;
+    } else {
+      return MediaQuery.of(context).size.width * 0.20;
+    }
   }
 }
